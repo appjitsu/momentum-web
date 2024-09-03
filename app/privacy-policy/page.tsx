cdca9141cb54e9eef9b5eb2f2b5137ae6cc32bd2
Jason Cochran <jlcochran2013@gmail.com>
2024-09-03 10:31:59 -0500

    privacy and terms pages

--- app/privacy-policy/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from '@/components/Container';
-
-export default function PrivacyPolicy() {
-  return (
-    <Container>
-      <div></div>
-    </Container>
-  );
-}
--- app/terms-of-use/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from '@/components/Container';
-
-export default function TermsOfUse() {
-  return (
-    <Container>
-      <div></div>
-    </Container>
-  );
-}
